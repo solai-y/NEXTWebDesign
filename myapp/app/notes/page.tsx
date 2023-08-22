2be0b9140a0491fe9f7c9436cec6543e76b14b13
solai-y <solaiy17112001@gmail.com>
2023-08-22 14:05:54 +0800

    Edited CSS and setting up notes page

---
+++ myapp/app/notes/page.tsx
@@ -0,0 +1,14 @@
+
+async function getNotes() {
+    const res = await fetch();
+}
+
+export default async function NotesPage(){
+
+    return (
+        <div>
+            <h1>Notes</h1>
+        </div>
+    );
+
+}
