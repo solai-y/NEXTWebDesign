e6b33475d7938805722c4d9554ab0aeff1ea9a85
solai-y <solaiy17112001@gmail.com>
2023-08-23 13:04:32 +0800

    Errors
    Current version of the app still doesnt allow the note to be created on the SQL database and then the loading of the page of the notes information is not working correctly

--- myapp/app/notes/CreateNote.tsx
+++ myapp/app/notes/CreateNote.tsx
@@ -3,8 +3,8 @@
 import {useState} from 'react';
 
 export default function CreateNote() {
-    const [title, setTitle] = useState('');
-    const [content, SetContent] = useState('');
+    const [Title, setTitle] = useState('');
+    const [Content, SetContent] = useState('');
 
     const create = async () => {
         await fetch('http://127.0.0.1:8090/api/collections/Notes/records',
@@ -14,8 +14,8 @@ export default function CreateNote() {
                     'Content-Type': 'application/json',
                 }, 
                 body: JSON.stringify({
-                    title,
-                    content,
+                    Title,
+                    Content,
                 }),
             }
         );
@@ -26,8 +26,8 @@ export default function CreateNote() {
     return (
         <form onSubmit={create}>
             <h3>Create Note</h3>
-            <input type="text" placeholder='title' value={title} onChange={(e)=>setTitle(e.target.value)}/>
-            <textarea placeholder='content' value={content} onChange={(e)=>SetContent(e.target.value)}></textarea>
+            <input type="text" placeholder='title' value={Title} onChange={(e)=>setTitle(e.target.value)}/>
+            <textarea placeholder='content' value={Content} onChange={(e)=>SetContent(e.target.value)}></textarea>
             <button type="submit">Create Note</button>
         </form>
     );  
